33146144fd77718438893ba3a4350def9b8e02bf
John Guibas <john@succinct.xyz>
2023-10-31 12:32:39 -0700

    feat: whitelist column

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ usage of the platform, including:
 2. Deploying your verifier onchain so that your application can verify proofs.
 3. Requesting a proof onchain and having the result delivered to your smart contract.
 
-**This demo will deploy an oracle that returns the sum of the balances of the first 128 Ethereum 
+**This demo will deploy an oracle that returns the sum of the balances of the first four Ethereum 
 validators.**  The entire result will be computed using zero-knowledge proofs, meaning that there
-are absolutely no trust assumptions (besides implementation bugs).
+are absolutely no trust assumptions (beyond implementation bugs).
 
 The important parts of the core logic are described below using our proving framework [plonky2x](). The platform can 
 support any proving system, ranging from zkVMs to circuit libraries like Halo2, Gnark, and more. The source code attached with this project is available on [GitHub]().
@@ -20,15 +20,23 @@ support any proving system, ranging from zkVMs to circuit libraries like Halo2, 
 ## Source Code
 
 ```rust
-impl Circuit for Sha256 {
+impl Circuit for ValidatorBalanceOracle {
     fn define<L: PlonkParameters<D>, const D: usize>(builder: &mut CircuitBuilder<L, D>)
     where
         <<L as PlonkParameters<D>>::Config as GenericConfig<D>>::Hasher:
             AlgebraicHasher<<L as PlonkParameters<D>>::Field>,
     {
-        let input = builder.evm_read::<Bytes32Variable>();
-        let hash = builder.sha256(&input.as_bytes());
-        builder.evm_write::<Bytes32Variable>(hash);
+        let block_root = builder.evm_read::<Bytes32Variable>();
+        let balances = builder.beacon_get_balances(block_root);
+
+        let mut sum = builder.zero::<U64Variable>();
+        for i in 0..4 {
+            let idx = builder.constant::<U64Variable>(i);
+            let balance = builder.beacon_get_balance(balances, idx);
+            sum = builder.add(sum, balance);
+        }
+
+        builder.evm_write::<U64Variable>(sum);
     }
 }
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
 use plonky2::plonk::config::{AlgebraicHasher, GenericConfig};
 use plonky2x::backend::circuit::{Circuit, PlonkParameters};
 use plonky2x::backend::function::Plonky2xFunction;
-use plonky2x::prelude::{Bytes32Variable, CircuitBuilder};
+use plonky2x::prelude::{Bytes32Variable, CircuitBuilder, U64Variable};
 
 #[derive(Debug, Clone)]
-struct Sha256;
+struct ValidatorBalanceOracle;
 
-impl Circuit for Sha256 {
+impl Circuit for ValidatorBalanceOracle {
     fn define<L: PlonkParameters<D>, const D: usize>(builder: &mut CircuitBuilder<L, D>)
     where
         <<L as PlonkParameters<D>>::Config as GenericConfig<D>>::Hasher:
             AlgebraicHasher<<L as PlonkParameters<D>>::Field>,
     {
-        let input = builder.evm_read::<Bytes32Variable>();
-        let hash = builder.sha256(&input.as_bytes());
-        builder.evm_write::<Bytes32Variable>(hash);
+        let block_root = builder.evm_read::<Bytes32Variable>();
+        let balances = builder.beacon_get_balances(block_root);
+
+        let mut sum = builder.zero::<U64Variable>();
+        for i in 0..4 {
+            let idx = builder.constant::<U64Variable>(i);
+            let balance = builder.beacon_get_balance(balances, idx);
+            sum = builder.add(sum, balance);
+        }
+
+        builder.evm_write::<U64Variable>(sum);
     }
 }
 
 fn main() {
-    Sha256::entrypoint();
+    ValidatorBalanceOracle::entrypoint();
 }
 
 #[cfg(test)]
 mod tests {
+    use std::env;
+
     use plonky2x::prelude::DefaultParameters;
     use plonky2x::utils::bytes32;
 
@@ -32,36 +42,33 @@ mod tests {
     type L = DefaultParameters;
     const D: usize = 2;
 
-    const EXAMPLE_INPUT: &str =
-        "0x49869d23ba93a746cc8ea649a48bb6c4b2159cf3a71aef492af63dac27522c9f";
-
-    const EXAMPLE_OUTPUT: &str =
-        "0x34327c0f2222f180f511e1375af48b83145b50b60d558c9951defb99f2021326";
+    const EXAMPLE_BLOCK_ROOT: &str =
+        "0xc366a826d730e5e8767a3b13f81cbf8ffa10a269272232b9243337637aef6dc7";
 
     #[test]
     fn test_circuit() {
         env_logger::try_init().unwrap_or_default();
         dotenv::dotenv().ok();
+        env::set_var("CONSENSUS_RPC_1", "https://beaconapi.succinct.xyz");
 
         // Build the circuit.
         let mut builder = CircuitBuilder::<L, D>::new();
-        Sha256::define(&mut builder);
+        ValidatorBalanceOracle::define(&mut builder);
         let circuit = builder.build();
 
         // Generate input.
         let mut input = circuit.input();
-        input.evm_write::<Bytes32Variable>(bytes32!(EXAMPLE_INPUT));
+        input.evm_write::<Bytes32Variable>(bytes32!(EXAMPLE_BLOCK_ROOT));
 
         // Generate the proof and verify.
         let (proof, mut output) = circuit.prove(&input);
         circuit.verify(&proof, &input, &output);
 
         // Read output.
-        let output = output.evm_read::<Bytes32Variable>();
-        let expected_output = bytes32!(EXAMPLE_OUTPUT);
-        assert_eq!(output, expected_output);
+        let output = output.evm_read::<U64Variable>();
+        println!("{}", output);
 
         // Test circuit serialization.
-        Sha256::test_serialization::<L, D>();
+        ValidatorBalanceOracle::test_serialization::<L, D>();
     }
 }
